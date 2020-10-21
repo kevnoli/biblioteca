775163e117d3e09eca070d05a7c480a71be16b94
Kevin Oliveira <kevinols08@gmail.com>
2020-10-20 23:53:52 -0300

    Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,16 +3,31 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
+    * Run the database seeders.
+    *
+    * @return void
+    */
     public function run()
     {
-        // User::factory(10)->create();
+        $a = '';
+        for ($i = 0; $i<11; $i++) 
+        {
+            $a .= mt_rand(0,9);
+        }
+        DB::table('administradores')->insert([
+            'cpf' => $a,
+            'nome' => Str::random(10),
+            'email' => Str::random(10).'@gmail.com',
+            'senha' => Hash::make('123456'),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
     }
 }

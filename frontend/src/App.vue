dc039cb68bd4e2665fccea580bcd3994f0799e7c
Kevin Oliveira <kevinols08@gmail.com>
2021-04-28 10:43:51 -0300

    Vuejs, vue-router e vuetify

---
+++ frontend/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app>
+  <v-navigation-drawer app>
+  </v-navigation-drawer>
+
+  <v-app-bar app>
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-app-bar-title>Biblioteca</v-app-bar-title>
+  </v-app-bar>
+
+  <v-main>
+    <v-container fluid>
+      <router-view></router-view>
+    </v-container>
+  </v-main>
+</v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
